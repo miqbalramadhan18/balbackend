3a4e302fea4553ad4ad2818563530cbc98459ad7
miqbalramadhan18 <miqbalr18@gmail.com>
2023-06-27 16:14:03 +0700

    update

---
+++ iqbal.go
@@ -0,0 +1,104 @@
+package balbackend
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoString string = os.Getenv("MONGOSTRING")
+
+func MongoConnect(dbname string) (db *mongo.Database) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MongoString))
+	if err != nil {
+		fmt.Printf("MongoConnect: %v\n", err)
+	}
+	return client.Database(dbname)
+}
+
+func InsertOneDoc(db string, collection string, doc interface{}) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection(collection).InsertOne(context.TODO(), doc)
+	if err != nil {
+		fmt.Printf("InsertOneDoc: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+func InsertJalurPenerimaan(db string, jalurpenerimaan JalurPenerimaan) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection("jalurpenerimaan").InsertOne(context.TODO(), jalurpenerimaan)
+	if err != nil {
+		fmt.Printf("InsertJalurPenerimaan: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+func InsertInformasi(db string, informasi Informasi) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection("informasi").InsertOne(context.TODO(), informasi)
+	if err != nil {
+		fmt.Printf("InsertInformasi: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+func InsertBiaya(db string, biaya Biaya) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection("biaya").InsertOne(context.TODO(), biaya)
+	if err != nil {
+		fmt.Printf("InsertBiaya: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+// func InsertAbout(db string, about About) (insertedID interface{}) {
+// 	insertResult, err := MongoConnect(db).Collection("about").InsertOne(context.TODO(), about)
+// 	if err != nil {
+// 		fmt.Printf("InsertAbout: %v\n", err)
+// 	}
+// 	return insertResult.InsertedID
+// }
+// func InsertContactus(db string, contacus Contactus) (insertedID interface{}) {
+// 	insertResult, err := MongoConnect(db).Collection("contactus").InsertOne(context.TODO(), contacus)
+// 	if err != nil {
+// 		fmt.Printf("InsertContactus: %v\n", err)
+// 	}
+// 	return insertResult.InsertedID
+// }
+func GetDataJalurPenerimaan(stats string) (data []JalurPenerimaan) {
+	user := MongoConnect("qobel").Collection("jalurpenerimaan")
+	filter := bson.M{"jalurtes": stats}
+	cursor, err := user.Find(context.TODO(), filter)
+	if err != nil {
+		fmt.Println("GetJalurPenerimaan :", err)
+	}
+	err = cursor.All(context.TODO(), &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+func GetDataInformasi(stats string) (data []Informasi) {
+	user := MongoConnect("qobel").Collection("informasi")
+	filter := bson.M{"catatan": stats}
+	cursor, err := user.Find(context.TODO(), filter)
+	if err != nil {
+		fmt.Println("GetDataInformasi :", err)
+	}
+	err = cursor.All(context.TODO(), &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+func GetDataBiaya(stats string) (data []Biaya) {
+	user := MongoConnect("qobel").Collection("biaya")
+	filter := bson.M{"biayasemester": stats}
+	cursor, err := user.Find(context.TODO(), filter)
+	if err != nil {
+		fmt.Println("GetDatabiaya :", err)
+	}
+	err = cursor.All(context.TODO(), &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+
---
+++ iqbal_test.go
@@ -0,0 +1,70 @@
+package balbackend
+
+import (
+	"fmt"
+	"testing"
+)
+
+// var MongoInfo = atdb.DBInfo{
+// 	DBString: MongoString,
+// 	DBName:   "qobel",
+// }
+
+// var MongoConn = atdb.MongoConnect(MongoInfo)
+
+// func TestInsertJalurPenerimaan(t *testing.T) {
+// 	dbname := "qobel"
+// 	jalurpenerimaan:= JalurPenerimaan{
+// 		ID:      primitive.NewObjectID(),
+// 		Jalurtanpates: "rapot",
+// 		Jalurtes: "Mandiri",
+// 		Beasiswa: "UTBK",
+// 		Status: "Masih Berlaku",
+// 	}
+// 	insertedID := InsertJalurPenerimaan(dbname, jalurpenerimaan)
+// 	if insertedID == nil {
+// 		t.Error("Failed to insert user")
+// 	}
+// }
+
+// func TestInsertInformasi(t *testing.T) {
+// 	dbname := "qobel"
+// 	informasi := Informasi{
+// 		ID:       primitive.NewObjectID(),
+// 		Alur: "Proses Pendaftaran",
+// 		Catatan: "catatan",
+// 	}
+// 	insertedID := InsertInformasi(dbname, informasi)
+// 	if insertedID == nil {
+// 		t.Error("Failed to insert informasi")
+// 	}
+// }
+
+// func TestInsertBiaya(t *testing.T) {
+// 	dbname := "qobel"
+// 	biaya := Biaya{
+// 		ID:     primitive.NewObjectID(),
+// 		Biayasemester: "7.700.000.00",
+// 	}
+// 	insertedID := InsertBiaya(dbname, biaya)
+// 	if insertedID == nil {
+// 		t.Error("Failed to insert biaya")
+// 	}
+// }
+
+func TestGetJalurPenerimaan(t *testing.T) {
+	stats := "jalurtes"
+	data := GetDataJalurPenerimaan(stats)
+	fmt.Println(data)
+}
+func TestGetDataInformasi(t *testing.T) {
+	stats := "catatan"
+	data := GetDataInformasi(stats)
+	fmt.Println(data)
+}
+
+func TestGetDataBiaya(t *testing.T) {
+	stats := "biayasemester"
+	data := GetDataBiaya(stats)
+	fmt.Println(data)
+}
